73e858d4519da764c1209d5797792a5763ede086
Artur <q79199467455q@gmail.com>
2022-07-19 12:11:51 +0300

    Add base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import legacy from '@vitejs/plugin-legacy'
 
 export default {
-  base: '/captcha/',
   plugins: [
     legacy({
       targets: ['defaults', 'not IE 11']
     })
-  ]
+  ],
+  base: '/captcha/',
 }
